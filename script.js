6199164457a3c546b2bcfddaa28e5a2e3f120dcc
Sara Adi <66746550+Sara-Adi@users.noreply.github.com>
2022-09-14 22:03:13 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const body = document.body
+
+const btnTheme = document.querySelector('.fa-moon-o')
+
+
+const addThemeClass = (bodyClass, btnClass) => {
+    body.classList.add(bodyClass)
+    btnTheme.classList.add(btnClass)
+}
+
+const getBodyTheme = localStorage.getItem('portfolio-theme')
+const getBtnTheme = localStorage.getItem('portfolio-btn-theme')
+
+addThemeClass(getBodyTheme, getBtnTheme)
+
+const isDark = () => body.classList.contains('dark')
+
+const setTheme = (bodyClass, btnClass) => {
+
+    body.classList.remove(localStorage.getItem('portfolio-theme'))
+    btnTheme.classList.remove(localStorage.getItem('portfolio-btn-theme'))
+
+    addThemeClass(bodyClass, btnClass)
+
+    localStorage.setItem('portfolio-theme', bodyClass)
+    localStorage.setItem('portfolio-btn-theme', btnClass)
+}
+
+const toggleTheme = () =>
+    isDark() ? setTheme('light', 'fa-moon-o') : setTheme('dark', 'fa-sun-o')
+
+btnTheme.addEventListener('click', toggleTheme)
+
+
+
+
+
+
+
+btnHamburger.addEventListener('click', displayList)
+
+
+document.addEventListener('scroll', scrollUp)
